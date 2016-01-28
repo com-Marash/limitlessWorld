1d62d63d8e27b22d79bcd29bf1c7bc47a67c4cc7
Arash Khosravi <akhosravi@mmm.com>
2016-01-28 15:13:08 -0500

    init commit

---
+++ src/main/java/com/marash/general/simpleWebService/App.java
@@ -0,0 +1,13 @@
+package com.marash.general.simpleWebService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
