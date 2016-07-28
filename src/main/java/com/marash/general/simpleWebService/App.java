34957e3241d1af4a6409ad78283cff0997425ba7
Arash Khosravi <arashsoft@gmail.com>
2016-07-28 17:10:38 -0400

    Remove junit Testing. Add SpringApplication boot.

--- src/main/java/com/marash/general/simpleWebService/App.java
+++ src/main/java/com/marash/general/simpleWebService/App.java
@@ -1,13 +1,14 @@
 package com.marash.general.simpleWebService;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Simple Web Service is running!" );
+        SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/marash/general/simpleWebService/authController.java
+++ src/main/java/com/marash/general/simpleWebService/authController.java
@@ -1,7 +1,5 @@
 package com.marash.general.simpleWebService;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
